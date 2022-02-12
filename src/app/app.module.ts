5442460733eaa2c55faa7d44e65777ca053a509f
Till Seifert <me@till.seifert.name>
2022-02-12 16:39:33 +0100

    Add bootstrap and ngx-store

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {FormsModule} from "@angular/forms";
+import { WebStorageModule } from 'ngx-store';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+	imports: [
+		BrowserModule,
+		FormsModule,
+  WebStorageModule.forRoot(),
+  BrowserAnimationsModule
+	],
   providers: [],
   bootstrap: [AppComponent]
 })
